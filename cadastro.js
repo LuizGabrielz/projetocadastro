d6686ed2404cd9f10b0f74b3c3cd757da2c2a658
Luiz Gabriel <luizgabrielz@gmail.com>
2022-11-16 16:49:54 -0300

    commit com relogio

--- cadastro.js
+++ cadastro.js
@@ -10,13 +10,13 @@ const iniciar = document.querySelector('.iniciar');
 const pausar = document.querySelector('.pausar');
 const finalizar = document.querySelector('.finalizar');
 let segundos = 0;
-let timer;
+let timer; 
 
 function iniciaRelogio() {
     timer = setInterval(function() {
         segundos++;
         relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000)
+    }, 1000) 
     
 }
  
@@ -25,44 +25,48 @@ iniciar.addEventListener('click', function(event) {
     clearInterval(timer);
     iniciaRelogio(); 
 });   
-
+ 
 pausar.addEventListener('click', function(event) {
     clearInterval(timer);
     relogio.classList.add('pausado');
 });
 
 finalizar.addEventListener('click', function(event) {
+ 
     
-
-});    
-
+});     
+ 
 $(function(){
 
     const tbody = document.querySelector(".teste");
 
     $(".finalizar").on("click", function(){
-     const form = document.querySelector('.form');
-        var tabelaatividade = [];
+     const form = document.querySelector('.form'); 
+
+        var tabelaatividade = []; 
 
         const usuario = form.querySelector('#usuario');
         const atividade = form.querySelector('#atividade');
         const tipoatividade = form.querySelector('#tipoatividade');
-      
         
+    
         tabelaatividade.push({
             usuario: usuario.value,
             atividade: atividade.value,
-            tipoatividade: tipoatividade.value
-
-        }); 
+            tipoatividade: tipoatividade.value, 
+            
+        
+        });
 
         var row = tbody.insertRow(0);
                 row.innerHTML = `
                 <tr>
                 <td>${usuario.value}</td> 
                 <td>${atividade.value}</td>  
-                <td>${tipoatividade.value}</td>  
+                <td>${tipoatividade.value}</td>   
+                <td>${relogio.innerHTML}</td>   
                 </tr> `    
     });  
 })
  
+   
