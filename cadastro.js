967a4c5c8c78d4f3085374cd201a01ba2b3fad5b
Luiz Gabriel <luizgabrielz@gmail.com>
2022-11-18 16:48:36 -0300

    quase pronto

--- cadastro.js
+++ cadastro.js
@@ -31,68 +31,74 @@ pausar.addEventListener('click', function(event) {
     clearInterval(timer);
     relogio.classList.add('pausado');
 });
-
-finalizar.addEventListener('click', function(event) {
- 
-    
-});     
-
-
-$(function(){
-
-    const tbody = document.querySelector(".teste");
-
-    $(".finalizar").on("click", function(){
-     const form = document.querySelector('.form'); 
-
-        const usuario = form.querySelector('#usuario');
-        const atividade = form.querySelector('#atividade');
-        const tipoatividade = form.querySelector('#tipoatividade');
-        
-    
-        tabelaatividade.push({
-            usuario: usuario.value,
-            atividade: atividade.value,
-            tipoatividade: tipoatividade.value, 
-            relogio: relogio.innerHTML
-            
-        
-        });
-
-        var row = tbody.insertRow(0); 
-                row.innerHTML = `
-                <tr>
-                <td>${usuario.value}</td> 
-                <td>${atividade.value}</td>  
-                <td>${tipoatividade.value}</td>   
-                <td>${relogio.innerHTML}</td>   
-                </tr> `    
-    });  
-}) 
-
-
-// funcao ordenar
-
-function Ordernar() {
-  var arrayOrdenado = array.sort()
   
-  const tbody = document.querySelector(".teste");
 
-  for  (var i = 0; i < arrayOrdenado.length; i++)  {
 
-  };
+
+var teste = 0;
+
+function salvarDados(){
+  var usuario  = document.getElementById("usuario").value;
+  var atividade     = document.getElementById("atividade").value;
+  var tipoatividade = document.getElementById("tipoatividade").value;
     
+  
+
+  if (!(usuario == '' || atividade == '' || tipoatividade == '' )) {  
+    var tr = document.createElement("tr");
+    var td1 = document.createElement("td");
+    var td2 = document.createElement("td"); 
+    var td3 = document.createElement("td");
+    var td4 = document.createElement("td");
+    var btn = document.createElement("button");                 
+    td1.innerHTML = usuario; 
+    td2.innerHTML = atividade;
+    td3.innerHTML = tipoatividade;
+    td4.innerHTML = relogio.innerHTML
+    btn.innerHTML = "deletar"; 
+      
+    btn.setAttribute('onclick', 'deletar(this)');
+
+    tr.appendChild(td1); 
+    tr.appendChild(td2);
+    tr.appendChild(td3);
+    tr.appendChild(td4);        
+    tr.appendChild(btn);
+    tr.id = 'linha' + teste; 
+    teste++;
+    document.getElementById("tabela").appendChild(tr);
+
+    limparCampos();
+  }else {
+    alert('Todos os campos precisam estar preenchidos !!');
+    }
+}
+function limparCampos(){
+  var usuario     = document.getElementById("nome");
+  var atividade      = document.getElementById("valor");
+  var tipoatividade = document.getElementById("vencimento");
+  relogio.innerHTML
+  
+  
+  usuario.value       = '';
+  atividade.value      = '';
+  tipoatividade.value = '';
+  relogio.innerHTML  = '';
+}
+
+function deletar(botao) {
+  var tabela = document.getElementById('tabela');
+  
+ tabela.deleteRow(botao.parentNode.rowIndex); 
+
+}
+ 
 
 
-  var row = tbody.insertRow(0); 
-    row.innerHTML = `
-    <tr>
-    <td>${usuario}</td> 
-    <td>${atividade}</td>  
-    <td>${tipoatividade}</td>   
-    <td>${relogio}</td>   
-    </tr> `    
-  }
+ 
+
+
+
 
 
 
